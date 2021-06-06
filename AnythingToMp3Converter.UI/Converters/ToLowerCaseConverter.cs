3a59bf652c852a814ceef376ee9011cf4fe612d9
Chris Van den Broeck <ChrisVdB36@hotmail.com>
2021-06-06 19:17:23 +0200

    Converter added for text convertion (from binded values)

---
+++ AnythingToMp3Converter.UI/Converters/ToLowerCaseConverter.cs
@@ -0,0 +1,18 @@
+﻿namespace AnythingToMp3Converter.UI.Converters
+{
+    using AnythingToMp3Converter.UI.Enums;
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    internal class ToLowerCaseConverter : IValueConverter
+    {
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => value;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string text) return text.ToLower();
+            if (value is FileStatus status) return status.ToString().ToLower();
+            return value;
+        }
+    }
+}
