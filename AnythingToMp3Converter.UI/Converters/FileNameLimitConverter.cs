55bff90a9c1e310f56bbc39603fb02ccd5453fa8
Chris Van den Broeck <ChrisVdB36@hotmail.com>
2021-06-06 21:08:23 +0200

    FileNameLimitConverter added

---
+++ AnythingToMp3Converter.UI/Converters/FileNameLimitConverter.cs
@@ -0,0 +1,21 @@
+﻿namespace AnythingToMp3Converter.UI.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class FileNameLimitConverter : IValueConverter
+    {
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => value;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string fileName)
+            {
+                fileName = string.Concat(@"...\", fileName);
+                return fileName.Length > 61 ? string.Concat(fileName.Substring(0, 58), "...") : fileName;
+            }
+
+            return value;
+        }
+    }
+}
