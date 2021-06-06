f80fb77d9860883811fd8e464d3776ce3fdd2570
Chris Van den Broeck <ChrisVdB36@hotmail.com>
2021-06-06 18:25:57 +0200

    Action changed to FileStatus and type changed from string to enum

--- AnythingToMp3Converter.UI/Models/MediaFile.cs
+++ AnythingToMp3Converter.UI/Models/MediaFile.cs
@@ -1,10 +1,12 @@
 ﻿namespace AnythingToMp3Converter.UI.Models
 {
+    using AnythingToMp3Converter.UI.Enums;
+
     public class MediaFile
     {
         public string FilePath { get; set; }
         public string FileName { get; set; }
-        public string Action { get; set; }
+        public FileStatus FileStatus { get; set; }
         public int Progress { get; set; }
     }
 }
