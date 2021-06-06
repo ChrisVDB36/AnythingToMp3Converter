51aed1c2457ad2f16081a3f95bbc3f4cca7598af
Chris Van den Broeck <ChrisVdB36@hotmail.com>
2021-06-06 18:53:07 +0200

    Name and method conventions set properly

--- AnythingToMp3Converter.UI/MainWindow.xaml.cs
+++ AnythingToMp3Converter.UI/MainWindow.xaml.cs
@@ -30,27 +30,27 @@
         private readonly string _ffmpegFileName = "ffmpeg";
         private readonly string _ffprobeFileName = "ffprobe";
 
-        // Update controls on listview changes
+        // ListView event handler: update controls on listview changes
         private void OnListViewSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            btnDelete.IsEnabled = lvMediaFiles.SelectedItem is MediaFile;
+            deleteMediaFileButton.IsEnabled = mediaFilesListView.SelectedItem is MediaFile;
         }
 
-        // Set output folder path
-        private void BtnSetOutputFolderPath(object sender, RoutedEventArgs e)
+        // Button event handler: set output folder path
+        private void OnSetOutputFolderPathButtonClick(object sender, RoutedEventArgs e)
         {
             using (var dialog = new FolderBrowserDialog())
             {
                 if (dialog.ShowDialog().Equals(System.Windows.Forms.DialogResult.OK))
                 {
                     string selectedPath = dialog.SelectedPath;
-                    if (Directory.Exists(selectedPath)) txtOutputFolderPath.Text = selectedPath;
+                    if (Directory.Exists(selectedPath)) outputFolderPathTextBox.Text = selectedPath;
                 }
             }
         }
 
-        // Add files to listview
-        private void BtnBrowseMediaFiles(object sender, RoutedEventArgs e)
+        // Button event handler: add files to listview (inserted as MediaFile)
+        private void OnAddFilesButtonClick(object sender, RoutedEventArgs e)
         {
             var openFileDialog = new OpenFileDialog
             {
@@ -62,11 +62,11 @@
             // Get files
             if ((bool)openFileDialog.ShowDialog())
             {
-                List<string> fileList = openFileDialog.FileNames.ToList();
-                if (fileList.Count <= 0) return;
+                List<string> files = openFileDialog.FileNames.ToList();
+                if (files.Count <= 0) return;
 
                 // Go tru all selected files
-                foreach (var file in fileList)
+                foreach (var file in files)
                 {
                     var mediaFile = new MediaFile
                     {
@@ -76,38 +76,38 @@
                         Progress = 0
                     };
 
-                    if (!lvMediaFiles.Items.Contains(mediaFile))
+                    if (!mediaFilesListView.Items.Contains(mediaFile))
                     {
-                        lvMediaFiles.Items.Add(mediaFile);
+                        mediaFilesListView.Items.Add(mediaFile);
                     }
                 }
 
-                btnClear.IsEnabled = lvMediaFiles.Items.Count > 0;
+                clearConverterButton.IsEnabled = mediaFilesListView.Items.Count > 0;
             }
         }
 
-        // Remove selected file from listview
-        private void BtnRemoveSelectedFile(object sender, RoutedEventArgs e)
+        // Button event handler: remove selected mediafile from listview
+        private void OnRemoveMediaFileButtonClick(object sender, RoutedEventArgs e)
         {
-            if (lvMediaFiles.SelectedItem is MediaFile mediaFile)
+            if (mediaFilesListView.SelectedItem is MediaFile mediaFile)
             {
-                lvMediaFiles.Items.Remove(mediaFile);
-                btnClear.IsEnabled = lvMediaFiles.Items.Count > 0;
+                mediaFilesListView.Items.Remove(mediaFile);
+                clearConverterButton.IsEnabled = mediaFilesListView.Items.Count > 0;
             }
         }
 
-        // Clear entire listview
-        private void BtnClearList(object sender, RoutedEventArgs e)
+        // Button event handler: clear entire listview
+        private void OnClearConverterButtonClick(object sender, RoutedEventArgs e)
         {
-            if (lvMediaFiles.Items.Count > 0) lvMediaFiles.Items.Clear();
-            btnClear.IsEnabled = false;
-            btnDelete.IsEnabled = false;
+            if (mediaFilesListView.Items.Count > 0) mediaFilesListView.Items.Clear();
+            clearConverterButton.IsEnabled = false;
+            deleteMediaFileButton.IsEnabled = false;
         }
 
-        // Convert files
-        private async void BtnConvertFilesAsync(object sender, RoutedEventArgs e)
+        // Button event handler: convert files
+        private async void OnStartConvertingButtonClickAsync(object sender, RoutedEventArgs e)
         {
-            string outputFolderPath = txtOutputFolderPath.Text;
+            string outputFolderPath = outputFolderPathTextBox.Text;
 
             // First check if output folder is set
             if (string.IsNullOrWhiteSpace(outputFolderPath) || !Directory.Exists(outputFolderPath))
@@ -117,17 +117,17 @@
             }
 
             // Dont continue if there are no media files found in listview
-            List<MediaFile> fileList = lvMediaFiles.Items.OfType<MediaFile>().ToList();
+            List<MediaFile> fileList = mediaFilesListView.Items.OfType<MediaFile>().ToList();
             if (fileList.Count > 0)
             {
                 MessageBox.Show("No files found to convert, converting aborted!", "No files found");
                 return;
             }
 
-            btnClear.IsEnabled = false;
-            btnDelete.IsEnabled = false;
-            btnConvertFiles.IsEnabled = false;
-            btnAddFiles.IsEnabled = false;
+            clearConverterButton.IsEnabled = false;
+            startConvertingButton.IsEnabled = false;
+            deleteMediaFileButton.IsEnabled = false;
+            addMediaFilesButton.IsEnabled = false;
 
             string alertMessage = string.Empty;
             int totalFailed = 0;
@@ -184,15 +184,15 @@
             if (totalFailed > 0) alertMessage = $"\n{totalFailed} {(totalFailed == 1 ? "file" : "files")} failed to convert, it is possible that the file is either corrupt or too large.";
             MessageBox.Show(string.Concat("Converting completed.", alertMessage), "DONE!", MessageBoxButtons.OK);
 
-            btnClear.IsEnabled = true;
-            btnConvertFiles.IsEnabled = true;
-            btnAddFiles.IsEnabled = true;
+            clearConverterButton.IsEnabled = true;
+            startConvertingButton.IsEnabled = true;
+            addMediaFilesButton.IsEnabled = true;
         }
 
         // Refresh listview when items are been updated
         private void RefreshListView()
         {
-            Dispatcher.Invoke(() => lvMediaFiles.Items.Refresh());
+            Dispatcher.Invoke(() => mediaFilesListView.Items.Refresh());
         }
 
         // Verify if ffmpeg and ffprobe are installed
@@ -204,7 +204,7 @@
                 string path = AppDomain.CurrentDomain.BaseDirectory;
                 if (!Directory.EnumerateFiles(path).Count(f => f.Contains(_ffmpegFileName) || f.Contains(_ffprobeFileName)).Equals(2))
                 {
-                    cpUpdater.Visibility = Visibility.Visible;
+                    ffmpegVerificationStackPanel.Visibility = Visibility.Visible;
                     await FFmpegDownloader.GetLatestVersion(FFmpegVersion.Official, path);
 
                     // Remove unused folder (macOS files)
@@ -212,12 +212,12 @@
                     if (Directory.Exists(macOsFilesFolder)) Directory.Delete(macOsFilesFolder, true);
 
                     // Fade out effect after install completion
-                    cpUpdater.ContentTemplate = TryFindResource("FFmpegUpdaterCompleted") as DataTemplate;
-                    cpUpdater.BeginAnimation(OpacityProperty, new DoubleAnimation(0, TimeSpan.FromSeconds(2)));
+                    ffmpegVerificationStackPanel.ContentTemplate = TryFindResource("FFmpegUpdaterCompleted") as DataTemplate;
+                    ffmpegVerificationStackPanel.BeginAnimation(OpacityProperty, new DoubleAnimation(0, TimeSpan.FromSeconds(2)));
 
                     // Delay before hiding panel
                     await Task.Delay(2000);
-                    cpUpdater.Visibility = Visibility.Collapsed;
+                    ffmpegVerificationStackPanel.Visibility = Visibility.Collapsed;
                 }
 
             }
