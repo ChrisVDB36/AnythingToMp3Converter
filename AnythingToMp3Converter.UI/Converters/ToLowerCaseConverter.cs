2103b3aab01e3c13f7fa104c38cb7ea176d1f3ad
Chris Van den Broeck <ChrisVdB36@hotmail.com>
2021-06-06 19:19:46 +0200

    Check changes

--- AnythingToMp3Converter.UI/Converters/ToLowerCaseConverter.cs
+++ AnythingToMp3Converter.UI/Converters/ToLowerCaseConverter.cs
@@ -10,8 +10,8 @@
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => value;
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is string text) return text.ToLower();
             if (value is FileStatus status) return status.ToString().ToLower();
+            if (value is string text) return text.ToLower();
             return value;
         }
     }
