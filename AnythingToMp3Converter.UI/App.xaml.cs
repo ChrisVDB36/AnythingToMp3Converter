074259c92494f664a436c0e07cf5fd6b9cee5100
Chris Van den Broeck <ChrisVdB36@hotmail.com>
2021-06-06 17:44:17 +0200

    Colors added

--- AnythingToMp3Converter.UI/App.xaml.cs
+++ AnythingToMp3Converter.UI/App.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace AnythingToMp3Converter.UI
+﻿namespace AnythingToMp3Converter.UI
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+    using System.Windows;
+    public partial class App : Application { }
 }
