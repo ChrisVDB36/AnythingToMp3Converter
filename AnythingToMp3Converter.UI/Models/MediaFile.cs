7ea8233165759be52b7b460436498f50fdec304a
Chris Van den Broeck <ChrisVdB36@hotmail.com>
2021-06-06 17:35:17 +0200

    MediaFile model added

---
+++ AnythingToMp3Converter.UI/Models/MediaFile.cs
@@ -0,0 +1,10 @@
+﻿namespace AnythingToMp3Converter.UI.Models
+{
+    public class MediaFile
+    {
+        public string FilePath { get; set; }
+        public string FileName { get; set; }
+        public string Action { get; set; }
+        public int Progress { get; set; }
+    }
+}
